a191f276d08eafdd76f1f88a4e61da17cf7f2e66
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-05-23 19:16:04 +0530

    Added lecture 48 notes

---
+++ 48Class_and_Object.py
@@ -0,0 +1,13 @@
+class computer:
+    def congif(self):
+        print("Processor :{} , RAM :{}, Size :{}".format("i5","16gb","1TB"))
+
+
+comp1=computer()
+comp2=computer()
+computer.congif(comp1)
+computer.congif(comp2)
+
+comp1.congif()
+comp2.congif()
+
