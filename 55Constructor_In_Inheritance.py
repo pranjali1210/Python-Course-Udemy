7e6f424bde0ad427adf21afedf95e4ee2d60c263
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-05-24 11:42:46 +0530

    Added 55Constructor_In_Inheritance

---
+++ 55Constructor_In_Inheritance.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self):
+        print("In A init")
+
+class B:
+    def __init__(self):
+        print("In B init")
+
+class C(A,B):
+    def __init__(self):
+        super().__init__()
+        print("In C init")
+
+
+c1=C()
+c1.__init__()
