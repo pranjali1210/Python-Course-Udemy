0163587afd2ccfa863045a102e70f3d4e4e1cac8
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-05-26 13:16:23 +0530

    Added File 75 Bubble Sort .

--- 75Bubble_Sort.py
+++ 75Bubble_Sort.py
@@ -1,5 +1,5 @@
 def sort(list):
-    for i in range(len(list)-1,0,-1):
+    for i in range(0,len(list)-1,1):
         for j in range(i):
             if list[j]>list[j+1]:
                 temp=list[j]
