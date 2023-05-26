e0e76b0ed1599e083e6a0693ec1c85aba194b2e3
pranjali1210 <roll.rbtl20it176@gmail.com>
2023-05-26 12:40:28 +0530

    Added File 74Binary_Search .

---
+++ 74Binary_Search.py
@@ -0,0 +1,22 @@
+pos=-1
+def BinarySearch(list,n):
+    l = 0
+    u = len(list) - 1
+    while l<=u:
+        mid=(l+u)//2
+        if list[mid]==n:
+            globals() ['pos']=mid
+            return  True
+        else:
+            if list[mid]<n:
+                l=mid
+            else:
+                u=mid
+    return  False
+
+list=[4,5,34,65,251,999]
+n=int(input("Enter Element to Search : "))
+if BinarySearch(list,n):
+    print("Element found at : ",pos+1)
+else:
+    print("Element Not Found .")
